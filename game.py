c4cf2b5845340fd07f47c28dcfdc10e0972b06ba
Perry Guloien <perryaguloien@gmail.com>
2022-02-02 13:39:25 -0500

    Make Plan for Requirements

--- game.py
+++ game.py
@@ -5,3 +5,29 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+# ASK FOR A USER INPUT AND WELCOME TO GAME
+
+print("Rock, Paper, Scissors, Shoot!")
+
+u = input("Please choose one of: 'rock', 'paper', 'scissors': ")
+
+print("USER CHOSE:", u)
+
+# VALIDATE THE USER CHOICE
+
+
+
+
+# COMPUTER CHOICE
+
+
+
+
+# DETERMINE THE WINNER
+
+
+
+
+
+# FINAL RESULTS
