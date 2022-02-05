22b65cbfb7b654acae62b24a1884b843b95e1b81
Perry Guloien <perryaguloien@gmail.com>
2022-02-04 20:40:36 -0500

    attempt at the bonus challenges

--- game.py
+++ game.py
@@ -5,14 +5,16 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+import os
+from dotenv import load_dotenv
 
-# ASK FOR A USER INPUT AND WELCOME TO GAME
+load_dotenv()
+
+player_name = os.getenv("PLAYER_NAME", default="Player One")
 
 print("Rock, Paper, Scissors, Shoot!")
 
-n = input("What is your name? ")
-
-print("Welcome to the game,",n)
+print("Welcome to the game, ",player_name)
 
 u = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
@@ -23,17 +25,17 @@ s = ["SCISSORS","scissors","Scissors"]
 #could have also put print("USER CHOSE:" u) but wanted to unify case
 
 if u in r:
-    u = "rock"
-    print("USER CHOSE: rock")
+        u = "rock"
+        print("USER CHOSE: rock")
 elif u in p:
-    u = "paper"
-    print("USER CHOSE: paper")
+        u = "paper"
+        print("USER CHOSE: paper")
 elif u in s:
-    u = "scissors"
-    print("USER CHOSE: scissors")
+        u = "scissors"
+        print("USER CHOSE: scissors")
 else:
-    print("Invalid selection. Please try again!")
-    quit()
+        print("Invalid selection. Please try again!")
+        quit()
 
 # VALIDATE THE USER CHOICE
 
@@ -56,22 +58,23 @@ print("COMPUTER CHOSE:", c)
 # DETERMINE THE WINNER
 
 if u == c:
-    print("It's a draw! You both chose",u)
+        print("It's a draw! You both chose",u)
 elif u in r:
-    if c in p:
-        print("Sorry! Paper covers rock. You lose.")
-    else:
-        print("Congrats! Rock crushes scissors. You won!")
+        if c in p:
+            print("Sorry! Paper covers rock. You lose.")
+        else:
+            print("Congrats! Rock crushes scissors. You won!")
 elif u in p:
-    if c in r:
-        print("Congrats! Paper covers rock. You won!")
-    else:
-        print("Sorry! Scissors cuts paper. You lose.")
+        if c in r:
+            print("Congrats! Paper covers rock. You won!")
+        else:
+            print("Sorry! Scissors cuts paper. You lose.")
 elif u in s:
-    if c in r:
-        print("Sorry! Rock crushes scissors. You lose.")
-    else:
-        print("Congrats! Scissors cuts paper. You won!")
+        if c in r:
+            print("Sorry! Rock crushes scissors. You lose.")
+        else:
+            print("Congrats! Scissors cuts paper. You won!")
+
 
 print("Thank you for playing! Please play again!")
 
